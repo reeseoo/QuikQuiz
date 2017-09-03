ec3f342277cc729b8b7b08c0d99408bd68a3234f
reeseoo <reesejmorris@gmail.com>
2017-09-03 16:55:51 +0100

    Merged changes.

--- src/menu.js
+++ src/menu.js
@@ -6,15 +6,8 @@ import { Button } from 'react-bootstrap';
 const MenuButton = (props) => {
   return (
     <div className="menu-button">
-<<<<<<< HEAD
-      <Link to={ props.link } 
-            onClick={ console.log("onClick works") } 
-            >
-                <Button bsStyle="success">{props.name}</Button>
-=======
-      <Link to={ props.link } >
-        <Button>{props.name}</Button>
->>>>>>> c54ed4b7338782338a0b7ca5ae98c346dec6ce8b
+      <Link to={ props.link } onClick={ console.log("onClick works") }>
+        <Button bsStyle="success">{props.name}</Button>
       </Link>
     </div>
   )
