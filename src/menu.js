ff6d7102df24d43af5084cc4cff19e3bb2c3de09
reeseoo <reesejmorris@gmail.com>
2017-09-03 12:48:29 +0100

    Added menu component

---
+++ src/menu.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+  /*
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+  */
+
+  render() {
+    return (
+      <div>
+        <h1>Quik Quiz</h1>
+      </div>
+    );
+  }
+}
+
+export default Menu;
