4bd7c3867981cb8f832f61690410f99a63ffa7cc
perjermer <pierce.morris1998@gmail.com>
2017-09-03 13:44:45 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './app.js';
 import Menu from './menu.js';
 
+//ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(<Menu />, document.getElementById('app'));
