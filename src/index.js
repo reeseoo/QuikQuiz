f75676c6e5bb1180d5f4ec94a92943f7fc281137
reeseoo <reesejmorris@gmail.com>
2017-09-03 21:47:18 +0100

    Moved link button to a seperate component Added a back button to the help page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var routes = require('./routes');
 import { BrowserRouter, Route } from 'react-router-dom'
 import App from './app'
 
--- src/menu.js
+++ src/menu.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './css/menu.css';
-import { Button } from 'react-bootstrap';
-
-const MenuButton = (props) => {
-  return (
-    <div className="menu-button">
-      <Link to={ props.link } onClick={ console.log("onClick works") }>
-        <Button>{props.name}</Button>
-      </Link>
-    </div>
-  )
-}
+import LinkButton from './linkbutton';
 
 class Menu extends Component {
   render() {
     return (
       <div>
-        <MenuButton link="/game" name="Start"/>
-        <MenuButton link="/help" name="Help"/>
+        <LinkButton link="/game" name="Start"/>
+        <LinkButton link="/help" name="Help"/>
       </div>
     );
   }
